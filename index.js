9fde51ad3db18da8ca5ac7c7976c33887f611c6b
Wim Holvoet <holvoetwim@hotmail.com>
2019-09-23 21:02:10 +0200

    data done

--- index.js
+++ index.js
@@ -13,19 +13,34 @@ axios.get(COMP_URL).then((response) => {
 
 
         $('.ploegcalendar-list li').each(function(index,element)  {
+        	if(index){
+
+
+const splitter = ':';
+
         	const beker = $(element).hasClass('beker');
+
         	const nr = $(element).find('span.cal-nr').text();
         	const date = $(element).find('span.cal-date').text();
         	const lea = $(element).find('span.cal-league').text();
-        	const time = $(element).find('span.cal-time').text();
+        	let time = $(element).find('span.cal-time').text();
+        	if(beker){
+        		time = time.replace('.',':');
+        	}
 			const homeT = $(element).find('span.cal-place1').text();
         	const outT = $(element).find('span.cal-place2').text();
 
-        	const subject = `${homeT} vs ${outT} @ ${lea}`;
+        	//const matchStartDateTime = new Date(`${date} ${time}`);
 
-        	const line = `${subject};${date};${time};`
+        	const matchEndTime = `${Number(time.split(splitter)[0])+1}:${time.split(splitter)[1]}`;
+
+
+        	const subject = `${nr}: ${homeT} vs ${outT} @ ${lea}`;
+
+        	const line = `${subject};${date};${time};${date};${matchEndTime};false;${subject};${lea};false`
 
         	console.log(line);
+        }
 
 
         })
